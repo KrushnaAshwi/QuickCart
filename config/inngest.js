ef90d01ade714ad96122e63a12d2d51035455b79
KrushnaAshwi <130756816+KrushnaAshwi@users.noreply.github.com>
2025-07-21 22:35:03 +0530

    Fix path alias and import for User model

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectionSB from "./db";
-import User from "@models/User"; // ✅
+// import User from "@models/User"; // ✅
+import User from "@/models/User"; // ✅ correct
 
 export const inngest = new Inngest({ id: "quickcart-next" });
 
