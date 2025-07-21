8cc37e2537e5ccef7c9a30e46747bed86026f8c2
KrushnaAshwi <130756816+KrushnaAshwi@users.noreply.github.com>
2025-07-21 22:20:31 +0530

    inngestCng2

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import connectionSB from "./db";
-import User from "./models/User"; // ✅ You forgot this import
+import User from "../models/User"; // ✅ CORRECT PATH BASED ON YOUR FOLDER STRUCTURE
 
 export const inngest = new Inngest({ id: "quickcart-next" });
 
